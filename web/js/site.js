5448db3e5c3bdca8c1b354ca3bc365eb4d31cd44
Povilas Balzaravicius Pawka <povilas@uber.com>
2015-05-25 09:12:48 +0300

    Forgot to add site.js :-) Fixed js.

---
+++ web/js/site.js
@@ -0,0 +1,48 @@
+var App = App || {};
+
+(function($){
+    App.Main = {
+        exec: function(controller, action) {
+
+            var ns = App,
+                action = ( action === undefined ) ? "init" : action;
+
+            controller = controller.charAt(0).toUpperCase() + controller.slice(1);
+            if (controller !== "" && ns[controller] && typeof ns[controller][action] == "function") {
+                ns[controller][action]();
+            }
+        },
+
+        init: function() {
+            var body = document.body,
+                controller = body.getAttribute("data-controller"),
+                action = body.getAttribute("data-action");
+            App.Main.exec(controller);
+            App.Main.exec(controller, action);
+        }
+    }; 
+
+    App.Talk = {
+        register: function() {
+            var $preview = $('#preview'),
+                $ta = $("#estina_bundle_homebundle_registertalk_description");
+
+            var empty = function() {
+                $preview.html('<em>Čia bus aprašymo peržiūra.</em>');
+            };
+
+            $ta.bind('input propertychange', function() {
+                var html = $.trim($(this).val());
+                if (0 === html.length) {
+                    empty();
+                } else {
+                    $preview.html(markdown.toHTML($(this).val()));
+                }
+            });
+            empty();
+        }
+    };
+
+    App.Main.init();
+
+})(jQuery);
