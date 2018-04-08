55683d74a5227a34550e96a90bc6c621dc542776
Petras Baukys <petrasb@gmail.com>
2018-04-08 23:12:17 +0300

    Pimped up landing | NON OPTIMIZED PHOTOS

--- src/Estina/Bundle/HomeBundle/Controller/DefaultController.php
+++ src/Estina/Bundle/HomeBundle/Controller/DefaultController.php
@@ -16,15 +16,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class DefaultController extends Controller
 {
+    
+    function filter_images($filename) {
+
+        if (strpos($filename, '.jpg') !== false) {
+            return true;
+        }
+
+        return false;
+    }
+
     /**
      * @Route("/", name="home")
      * @Template()
      */
     public function indexAction()
-    {
+    {   
+
+        $dir    = '../web/images/photos';
+        $photos = scandir($dir);
+
+        $photos = array_filter($photos, array($this, "filter_images"));
+
         return [
-            'feed' => $this->get('home.social_feed_service')->getFeed()
+            'feed' => $this->get('home.social_feed_service')->getFeed(),
+            'photos' => $photos,
         ];
     }
+
 
 }
